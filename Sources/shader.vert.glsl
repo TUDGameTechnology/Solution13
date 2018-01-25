7a57890d2d782bb98748028d7ae7664aa2c488bc
Polona Caserman <polona.caserman@gmail.com>
2018-01-25 13:14:57 +0100

    Update shader files

--- Sources/shader.vert.glsl
+++ Sources/shader.vert.glsl
@@ -1,13 +1,15 @@
-attribute vec3 pos;
-attribute vec2 tex;
-attribute vec3 nor;
-varying vec2 texCoord;
-varying vec3 normal;
+#version 450
+
+in vec3 pos;
+in vec2 tex;
+in vec3 nor;
+out vec2 texCoord;
+out vec3 normal;
 uniform mat4 P;
 uniform mat4 V;
 uniform mat4 M;
 
-void kore() {
+void main() {
 	gl_Position = P * V * M * vec4(pos.x, pos.y, pos.z, 1.0);
 	texCoord = tex;
 	normal = (V * M * vec4(nor, 0.0)).xyz;
